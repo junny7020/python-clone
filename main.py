8d8d29af2c235971bcfa50a80bf21e07fc38d63e
June Choi <junny7020@gmail.com>
2022-01-09 23:16:37 +0000

    #1.2 Tuples and Dicts

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
 
-print(days)
-days.append("Sun")
 print(days)
 print("Mon" in days)
 
 # There are two types of sequence type: list, tuple
+# tuple --> days = ("...","...","...","...")
+# list --> days = ["...","...","..."]
+
+june = {
+    "name": "June",
+    "age": "22",
+    "korean": True,
+    "fav_food": ["Chicken","Cooked Rice"]
+}
+print(june)
+june["undergradute"] = True
+print(june)
+# dic
