9880989735e256e6c0c274e76b6bbeea845c799a
June Choi <junny7020@gmail.com>
2022-01-09 22:55:48 +0000

    1.0 Data Types of Python

---
+++ main.py
@@ -0,0 +1,10 @@
+a_string = "like this"
+a_number = 3
+a_boolean = True
+d = False
+a_float = 3.14
+a_none = None
+
+print(type(a_boolean))
+# java script true,false
+# Snake Case (_) 는 Python 변수명 지정할때의 암묵적 약속이다.
