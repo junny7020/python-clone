5d7a20b18aa45bcd51faeeb02650c7c0c7ac2c07
June Choi <junny7020@gmail.com>
2022-01-15 21:24:26 +0000

    Indeed jobs location and links

--- indeed.py
+++ indeed.py
@@ -18,20 +18,27 @@ def extract_indeed_pages():
 #   for n in range(max_page):
 #     print(f"start={n*50}")
 
+def extract_job(html):
+    jobTitle = html.find("h2", {"class":"jobTitle"})
+    title = jobTitle.find("span").string
+    company = html.find("span", {"class":"companyName"}).string
+    if title == "new":
+        title = jobTitle.find_all("span")[1].string
+    location = html.find("div", {"class":"companyLocation"}).text
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, "link":f"https://www.indeed.com/viewjob?jk={job_id}"}
+
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("a",{"class":"fs-unmask"})
-    for result in results:
-        jobTitle = result.find("h2", {"class":"jobTitle"})
-        title = jobTitle.find("span").string
-
-        if title == "new":
-            title = jobTitle.find_all("span")[1].string
-        print(title)
+    for page in range(last_page):
+      print(f"Scraping page {page}")
+      result = requests.get(f"{URL}&start={page*LIMIT}")
+      soup = BeautifulSoup(result.text, "html.parser")
+      results = soup.find_all("a",{"class":"fs-unmask"})
+      for result in results:
+          job = extract_job(result)
+          jobs.append(job)
     return jobs
 
     
--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 last_indeed_page = extract_indeed_pages()
 print(last_indeed_page)
 
-extract_indeed_jobs(last_indeed_page)
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
+print(indeed_jobs)
